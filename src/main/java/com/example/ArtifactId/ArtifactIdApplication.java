da508ec986b90e516aafc7bd88df2970ad47d16a
unknown <c318016@PUSPN04P.hd00.UNICREDITGROUP.EU>
2017-08-21 16:44:45 +0200

    Spring Html CSS

---
+++ src/main/java/com/example/ArtifactId/ArtifactIdApplication.java
@@ -0,0 +1,15 @@
+package com.example.ArtifactId;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.system.ApplicationPidFileWriter;
+
+@SpringBootApplication
+public class ArtifactIdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication springApplication = new SpringApplication(ArtifactIdApplication.class);
+		springApplication.addListeners(new ApplicationPidFileWriter("Test.file"));
+		springApplication.run(args);
+	}
+}
