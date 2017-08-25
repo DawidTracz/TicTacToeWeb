3dfc8df3a9eca77624a748e9761012059df75858
unknown <dawid.tracz@o2.pl>
2017-08-25 15:20:05 +0200

    row and col set in html

--- src/main/resources/static/js/game.js
+++ src/main/resources/static/js/game.js
@@ -4,6 +4,12 @@ $(document).ready(function() {
         clickCount++;
         var chr = clickCount%2 == 0 ? 'X' : 'O';
         $(this).text(chr).attr('disabled','disabled');
-
     });
+    $('.game').each(function(index) {
+        console.log(index)
+        var btn = $($(this)[0]);
+        btn.attr("data-row", parseInt(index/3));
+        btn.attr("data-col", index%3);
+    });
+
 });
