2cbe827a47bdaef009ef9c4c60b3f870f605a0eb
unknown <dawid.tracz@o2.pl>
2017-08-28 14:36:16 +0200

    String with X and O

--- src/main/resources/static/js/game.js
+++ src/main/resources/static/js/game.js
@@ -1,15 +1,24 @@
 $(document).ready(function() {
     var clickCount = 1;
-    $('.game').click(function() {
-        clickCount++;
-        var chr = clickCount%2 == 0 ? 'X' : 'O';
-        $(this).text(chr).attr('disabled','disabled');
-    });
+          $('.game').click(function() {
+          var XOToWin="";
+          clickCount++;
+          var chr = clickCount%2 == 0 ? 'X' : 'O';
+          $(this).text(chr);
+          $(this).attr("symbol", chr);
+          $(this).text(chr).attr('disabled','disabled');
+                 $('.game').each(function(index) {
+                    if( $(this).attr("symbol")=='X'||$(this).attr("symbol")=='O'){
+                        XOToWin+=$(this).attr("symbol");
+                    } else{
+                           XOToWin+=" ";}
+                           });
+        console.log(XOToWin);
+     });
     $('.game').each(function(index) {
-        console.log(index)
-        var btn = $($(this)[0]);
-        btn.attr("data-row", parseInt(index/3));
-        btn.attr("data-col", index%3);
+        var button = $($(this)[0]);
+        button.attr("data-row", parseInt(index/3));
+        button.attr("data-col", index%3);
     });
 
 });
