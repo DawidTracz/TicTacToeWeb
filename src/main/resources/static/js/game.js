5ffd497ed603733a265b3fe362d18bebbc56fb5e
unknown <dawid.tracz@o2.pl>
2017-08-30 12:06:21 +0200

    apost

--- src/main/resources/static/js/game.js
+++ src/main/resources/static/js/game.js
@@ -1,29 +1,28 @@
 $(document).ready(function() {
     var clickCount = 1;
-    var model = { winner : XOToWin };
-    $('.game').click(function() {                   //After click on button function returns correct symbol in field
+    $('.game').click(function() {
           var XOToWin="";
           clickCount++;
           var chr = clickCount%2 == 0 ? 'X' : 'O';
-          var symbol = $(this).attr("symbol");
           $(this).text(chr);
           $(this).attr("symbol", chr);
           $(this).text(chr).attr('disabled','disabled');
-          XOToWin += (symbol == 'X' || symbol == 'O') ? symbol : ' ';
-          console.log(XOToWin);
+                $('.game').each(function(index) {
+                var symbol = $(this).attr("symbol");
+                XOToWin += (symbol == 'X' || symbol == 'O') ? symbol : ' ';
+                });
+        console.log(XOToWin);
+        var model = { winner : XOToWin };
+                $.post("/", model, function(data) {
+                alert('wyslano!');
+                });
     });
+
 
     $('.game').each(function(index) {
         var button = $($(this)[0]);
         button.attr("data-row", parseInt(index/3));
         button.attr("data-col", index%3);
     });
-
-    $.post("/", model, function(data) {
-    alert('wyslano!');
-    });
-
-
-
 
 });
