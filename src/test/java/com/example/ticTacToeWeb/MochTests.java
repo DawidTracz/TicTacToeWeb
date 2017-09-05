57407ab634079eb1f0fad35fa1a6f45fc2daf504
DawidT <dawid.tracz@o2.pl>
2017-09-05 16:55:12 +0200

    tests names fixed

---
+++ src/test/java/com/example/ticTacToeWeb/MochTests.java
@@ -0,0 +1,12 @@
+package com.example.ticTacToeWeb;
+
+import com.example.ticTacToeWeb.service.BasicWinnerService;
+import org.mockito.Mock;
+
+public class MochTests {
+@Mock
+private BasicWinnerService winnerService;
+
+
+
+}
--- src/test/java/com/example/ticTacToeWeb/SimpleTest.java
+++ src/test/java/com/example/ticTacToeWeb/SimpleTest.java
@@ -17,14 +17,14 @@ public class SimpleTest {
 
     //actual.action = expected
     @Test
-    public void Test1() {
+    public void selectWinner_should_select_X_when_all_symbols_equals_X() {
         TicTacModel model = TicTacModel.create().cols(3).rows(3).boardSymbols("XXXXXXXXX").build();
 
         Assert.assertEquals('X', winnerService.selectWinner(model));
     }
 
     @Test
-    public void Test2() {
+    public void selectWinner_should_select_X_when_all_symbols_equals_O() {
         TicTacModel model = TicTacModel.create().cols(3).rows(3).boardSymbols("OOOOOOOOO").build();
 
         Assert.assertEquals('O', winnerService.selectWinner(model));
@@ -33,42 +33,42 @@ public class SimpleTest {
 
     //Horizontal tests
     @Test
-    public void TestH1() {
+    public void selectWinner_should_select_X_when_all_symbols_in_first_row_equals_X() {
         TicTacModel model = TicTacModel.create().cols(3).rows(3).boardSymbols("XXX      ").build();
 
         Assert.assertEquals('X', winnerService.selectWinner(model));
     }
 
     @Test
-    public void TestH2() {
+    public void selectWinner_should_select_X_when_all_symbols_in_second_row_equals_X() {
         TicTacModel model = TicTacModel.create().cols(3).rows(3).boardSymbols("   XXX   ").build();
 
         Assert.assertEquals('X', winnerService.selectWinner(model));
     }
 
     @Test
-    public void TestH3() {
+    public void selectWinner_should_select_X_when_all_symbols_in_third_row_equals_X()  {
         TicTacModel model = TicTacModel.create().cols(3).rows(3).boardSymbols("      XXX").build();
 
         Assert.assertEquals('X', winnerService.selectWinner(model));
     }
 
     @Test
-    public void TestH4() {
+    public void selectWinner_should_select_O_when_all_symbols_in_first_row_equals_O()  {
         TicTacModel model = TicTacModel.create().cols(3).rows(3).boardSymbols("OOO      ").build();
 
         Assert.assertEquals('O', winnerService.selectWinner(model));
     }
 
     @Test
-    public void TestH5() {
+    public void selectWinner_should_select_O_when_all_symbols_in_second_row_equals_O() {
         TicTacModel model = TicTacModel.create().cols(3).rows(3).boardSymbols("   OOO   ").build();
 
         Assert.assertEquals('O', winnerService.selectWinner(model));
     }
 
     @Test
-    public void TestH6() {
+    public void selectWinner_should_select_O_when_all_symbols_in_third_row_equals_O() {
         TicTacModel model = TicTacModel.create().cols(3).rows(3).boardSymbols("      OOO").build();
 
         Assert.assertEquals('O', winnerService.selectWinner(model));
@@ -77,77 +77,77 @@ public class SimpleTest {
 
     //Horizontal tests
     @Test
-    public void TestV1() {
+    public void selectWinner_should_select_X_when_all_symbols_in_first_column_equals_X() {
         TicTacModel model = TicTacModel.create().cols(3).rows(3).boardSymbols("X  X  X  ").build();
 
         Assert.assertEquals('X', winnerService.selectWinner(model));
     }
 
     @Test
-    public void TestV2() {
+    public void selectWinner_should_select_X_when_all_symbols_in_second_column_equals_X() {
         TicTacModel model = TicTacModel.create().cols(3).rows(3).boardSymbols(" X  X  X ").build();
 
         Assert.assertEquals('X', winnerService.selectWinner(model));
     }
 
     @Test
-    public void TestV3() {
+    public void selectWinner_should_select_X_when_all_symbols_in_third_column_equals_X()  {
         TicTacModel model = TicTacModel.create().cols(3).rows(3).boardSymbols("  X  X  X").build();
 
         Assert.assertEquals('X', winnerService.selectWinner(model));
     }
 
     @Test
-    public void TestV4() {
+    public void selectWinner_should_select_O_when_all_symbols_in_first_column_equals_O()  {
         TicTacModel model = TicTacModel.create().cols(3).rows(3).boardSymbols("O  O  O  ").build();
 
         Assert.assertEquals('O', winnerService.selectWinner(model));
     }
 
     @Test
-    public void TestV5() {
+    public void selectWinner_should_select_O_when_all_symbols_in_second_column_equals_O()  {
         TicTacModel model = TicTacModel.create().cols(3).rows(3).boardSymbols(" O  O  O ").build();
 
         Assert.assertEquals('O', winnerService.selectWinner(model));
     }
 
     @Test
-    public void TestV6() {
+    public void selectWinner_should_select_O_when_all_symbols_in_third_column_equals_O()  {
         TicTacModel model = TicTacModel.create().cols(3).rows(3).boardSymbols("  O  O  O").build();
 
         Assert.assertEquals('O', winnerService.selectWinner(model));
     }
 
     @Test
-    public void TestC1() {
+    public void selectWinner_should_select_O_when_all_symbols_diagonal_equals_O_from_left_to_right() {
         TicTacModel model = TicTacModel.create().cols(3).rows(3).boardSymbols("O   O   O").build();
 
         Assert.assertEquals('O', winnerService.selectWinner(model));
     }
 
     @Test
-    public void TestC2() {
+    public void selectWinner_should_select_O_when_all_symbols_diagonal_equals_O_from_right_to_left() {
         TicTacModel model = TicTacModel.create().cols(3).rows(3).boardSymbols("  O O O  ").build();
 
         Assert.assertEquals('O', winnerService.selectWinner(model));
     }
 
     @Test
-    public void Test4x4H() {
+    public void selectWinner_should_select_X_when_all_symbols_in_first_row_equals_X_when_board_size_equals_4X4()  {
         TicTacModel model = TicTacModel.create().cols(4).rows(4).boardSymbols("XXXX            ").build();
 
         Assert.assertEquals('X', winnerService.selectWinner(model));
     }
 
     @Test
-    public void Test4x4Vert() {
+    public void selectWinner_should_select_X_when_all_symbols_in_first_column_equals_X_when_board_size_equals_4X4() {
         TicTacModel model = TicTacModel.create().cols(4).rows(4).boardSymbols("X   X   X   X   ").build();
 
         Assert.assertEquals('X', winnerService.selectWinner(model));
     }
 
     @Test
-    public void Test4x4cross() {
+    public void selectWinner_should_select_X_when_all_symbols_diagonal_X_from_left_to_right_when_board_size_equals_4X4() {
         TicTacModel model = TicTacModel.create().cols(4).rows(4).boardSymbols("X    X    X    X").build();
 
         Assert.assertEquals('X', winnerService.selectWinner(model));
